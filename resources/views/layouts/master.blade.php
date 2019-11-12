fa0cc39c08ee7205c622b0c4e41a2112c290d0d0
Lautaro López <lautaroml@hotmail.com>
2019-11-12 14:09:01 -0300

    Added logout nav-link

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -85,7 +85,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     </li>
 
                     <li class="nav-item has-treeview">
-                        <a href="#" class="nav-link active">
+                        <a href="#" class="nav-link">
                             <i class="nav-icon fas fa-cog"></i>
                             <p>
                                 Management
@@ -94,7 +94,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         </a>
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
-                                <a href="#" class="nav-link active">
+                                <a href="#" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Active Page</p>
                                 </a>
@@ -116,12 +116,16 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         </router-link>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">
+                        <a class="nav-link" href="{{ route('logout') }}"
+                           onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                             <i class="nav-icon fas fa-power-off"></i>
                             <p>
-                                Logout
+                                {{ __('Logout') }}
                             </p>
                         </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
                     </li>
                 </ul>
             </nav>
