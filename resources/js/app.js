414a14323bb2551809e11973a83109064b792a58
Lautaro López <lautaroml@hotmail.com>
2019-11-12 13:36:50 -0300

    Added vue-router and example components

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,23 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+import Dashboard from './components/Dashboard';
+import Profile from './components/Profile';
+
+let routes = [
+    { path: '/dashboard', component: Dashboard },
+    { path: '/profile', component: Profile }
+];
+
+const router = new VueRouter({
+    routes
+});
+
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router
 });
---
+++ resources/js/components/Profile.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>
+        Profile
+    </h1>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Hola munddo');
+        }
+    }
+</script>
