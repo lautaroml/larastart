eedbad111a47cd860a598c8e952e621464ffb504
Lautaro López <lautaroml@hotmail.com>
2019-11-13 12:29:25 -0300

    Added vform

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,6 +10,11 @@ import Dashboard from './components/Dashboard';
 import Profile from './components/Profile';
 import Users from './components/Users';
 
+import { Form, HasError, AlertError } from 'vform'
+window.form = Form;
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
+
 let routes = [
     { path: '/dashboard', component: Dashboard },
     { path: '/profile', component: Profile },
