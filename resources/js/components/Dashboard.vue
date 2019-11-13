5d78357c50826eba527cccacdfe1d4b6a8e1646d
Lautaro López <lautaroml@hotmail.com>
2019-11-13 12:20:53 -0300

    Changed Dashboard and Profile components HTML

--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -1,5 +1,18 @@
 <template>
-    <h1>
-        Dashboard
-    </h1>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">
+                        Dashboard Component
+                    </div>
+                    <div class="card-body">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad consectetur corporis debitis enim eos id inventore itaque laborum maxime molestiae quos tempora, unde veniam, veritatis! Architecto blanditiis esse illo!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
--- resources/js/components/Profile.vue
+++ resources/js/components/Profile.vue
@@ -1,13 +1,18 @@
 <template>
-    <h1>
-        Profile
-    </h1>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">
+                        Profile Component
+                    </div>
+                    <div class="card-body">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad consectetur corporis debitis enim eos id inventore itaque laborum maxime molestiae quos tempora, unde veniam, veritatis! Architecto blanditiis esse illo!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
-
-<script>
-    export default {
-        mounted() {
-            console.log('Hola munddo');
-        }
-    }
-</script>
