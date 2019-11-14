0c5c1dba2100079d9fb2418c9cf30f4b995c7aa7
Lautaro López <lautaroml@hotmail.com>
2019-11-14 13:32:21 -0300

    Added filters and moment-js

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,6 +9,7 @@ Vue.use(VueRouter);
 import Dashboard from './components/Dashboard';
 import Profile from './components/Profile';
 import Users from './components/Users';
+import moment from 'moment';
 
 import { Form, HasError, AlertError } from 'vform'
 window.Form = Form;
@@ -24,6 +25,14 @@ let routes = [
 const router = new VueRouter({
     mode: 'history',
     routes
+});
+
+Vue.filter('upText', function(text){
+    return text.charAt(0).toUpperCase() + text.slice(1);
+});
+
+Vue.filter('prityDate', function(date){
+    return moment(date).format('D/M/YYYY');
 });
 
 const app = new Vue({
